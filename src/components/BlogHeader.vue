3b6c4bd3e4826c2b2d10ca14272714afabb3d60f
wangwensheng <845082868@qq.com>
2019-07-25 18:41:31 +0800

    底边框

--- src/components/BlogHeader.vue
+++ src/components/BlogHeader.vue
@@ -1,6 +1,8 @@
 <template>
-  <div class="home">
-    <div>aaa</div>
+  <div>
+    <div class="container">
+
+    </div>
   </div>
 </template>
 <script lang='ts'>
@@ -13,5 +15,11 @@ export default class extends Vue {
 }
 </script>
 <style scoped lang="scss">
-
+.container{
+  background-color: #ffffff;
+  width: 100%;
+  height: 46px;
+  border-bottom: 1px solid #CCCCCC;
+  box-shadow: 1px 1px 2px #CBCBCB
+}
 </style>
