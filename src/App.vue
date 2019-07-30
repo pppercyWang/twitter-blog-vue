6d4d143fe5f9daa0efcd783caa08f7c3eeda42da
wangwensheng <845082868@qq.com>
2019-07-30 12:02:58 +0800

    s

--- src/App.vue
+++ src/App.vue
@@ -1,64 +1,57 @@
 <template>
   <div id="app">
-    <router-view/>
+    <router-view />
   </div>
 </template>
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import '../public/css/icon.css';
+import { Component, Vue } from "vue-property-decorator";
+import "../public/css/icon.css";
 @Component({
-  components: {
-  },
+  components: {}
 })
-export default class extends Vue {
-}
+export default class extends Vue {}
 </script>
 <style lang="scss">
 body {
-	margin: 0px;
-	padding: 0px;
-	/*background: url(assets/bg1.jpg) center !important;
+  margin: 0px;
+  padding: 0px;
+  /*background: url(assets/bg1.jpg) center !important;
 		background-size: cover;*/
-	// background: #1F2D3D;
-	font-family: Helvetica Neue, Helvetica, PingFang SC, Hiragino Sans GB, Microsoft YaHei, SimSun, sans-serif;
-	font-size: 14px;
-	-webkit-font-smoothing: antialiased;
+  // background: #1F2D3D;
+  font-family: Helvetica Neue, Helvetica, PingFang SC, Hiragino Sans GB,
+    Microsoft YaHei, SimSun, sans-serif;
+  font-size: 14px;
+  -webkit-font-smoothing: antialiased;
 }
-
 #app {
-	position: absolute;
-	top: 0px;
-	bottom: 0px;
-	width: 100%;
+  position: absolute;
+  top: 0px;
+  bottom: 0px;
+  width: 100%;
 }
-
-.el-submenu [class^=fa] {
-	vertical-align: baseline;
-	margin-right: 10px;
+.el-submenu [class^="fa"] {
+  vertical-align: baseline;
+  margin-right: 10px;
 }
-
-.el-menu-item [class^=fa] {
-	vertical-align: baseline;
-	margin-right: 10px;
+.el-menu-item [class^="fa"] {
+  vertical-align: baseline;
+  margin-right: 10px;
 }
-
 .toolbar {
-	background: #fffbfb;
-	padding: 10px;
-	// border:1px solid #dfe6ec;
-	margin: 10px 0px;
-	.el-form-item {
-		margin-bottom: 10px;
-	}
+  background: #fffbfb;
+  padding: 10px;
+  // border:1px solid #dfe6ec;
+  margin: 10px 0px;
+  .el-form-item {
+    margin-bottom: 10px;
+  }
 }
-
 .fade-enter-active,
 .fade-leave-active {
-	transition: all .2s ease;
+  transition: all 0.2s ease;
 }
-
 .fade-enter,
 .fade-leave-active {
-	opacity: 0;
+  opacity: 0;
 }
 </style>
