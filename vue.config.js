60c2c0532500e96c00414e729de9e67f8f03dba0
wangwensheng <845082868@qq.com>
2019-08-06 18:40:51 +0800

    ccc

--- vue.config.js
+++ vue.config.js
@@ -5,10 +5,23 @@ module.exports = {
         data: `
           @import "@/assets/css/_variable.scss";
         `
-      }
+      },
     }
   },
   configureWebpack: config => {
+    config.module.rules.push({
+      // 处理markdown文件
+      test: /\.md$/,
+      use: [
+        {
+          loader: "vue-loader"
+        },
+        {
+          loader: require.resolve("./src/common/markdownLoader")
+        }
+      ],
+    },
+    );
   },
   runtimeCompiler: true, 
   devServer: {
